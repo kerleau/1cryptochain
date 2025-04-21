ec74af0e80fb6d05c2c56d32afca7f9bb672797a
chaalej <alejandro.chavez@ext.ec.europa.eu>
2025-04-21 18:16:36 +0200

    A 101-6 Folder Structures created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const Blockchain = require('./blockchain');
-const PubSub = require('./pubsub');
+const PubSub = require('./app/pubsub');
 const request = require('request');
 
 const app = express();
