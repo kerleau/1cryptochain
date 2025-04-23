6442a8c7feb1daefdca8db384cb781b3d1e62be2
chaalej <alejandro.chavez@ext.ec.europa.eu>
2025-04-23 21:35:09 +0200

    A 101-7 & 101-8 To Miner class

--- app/pubsub.js
+++ app/pubsub.js
@@ -2,12 +2,14 @@ const redis = require('redis');
 
 const CHANNELS = {
     TEST: 'TEST',
-    BLOCKCHAIN: 'BLOCKCHAIN'
+    BLOCKCHAIN: 'BLOCKCHAIN',
+    TRANSACTION: 'TRANSACTION'
   };
   
   class PubSub {
-    constructor({ blockchain }) {
+    constructor({ blockchain, transactionPool }) {
       this.blockchain = blockchain;
+      this.transactionPool = transactionPool;
   
     this.publisher = redis.createClient({
         url: 'redis://localhost:12000'
@@ -29,8 +31,15 @@ const CHANNELS = {
   
       const parsedMessage = JSON.parse(message);
   
-      if (channel === CHANNELS.BLOCKCHAIN) {
-        this.blockchain.replaceChain(parsedMessage);
+      switch(channel) {
+        case CHANNELS.BLOCKCHAIN:
+          this.blockchain.replaceChain(parsedMessage);
+          break;
+        case CHANNELS.TRANSACTION:
+          this.transactionPool.setTransaction(parsedMessage);
+          break;
+        default:
+          return;
       }
     }
   
@@ -54,6 +63,14 @@ const CHANNELS = {
         message: JSON.stringify(this.blockchain.chain)
       });
     }
+
+    broadcastTransaction(transaction) {
+      this.publish({
+        channel: CHANNELS.TRANSACTION,
+        message: JSON.stringify(transaction)
+      })
+    }
+    
   }
   
   module.exports = PubSub;
