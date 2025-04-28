3717c8026d01ddd5b67bee9d8aec47663c0b541e
chaalej <alejandro.chavez@ext.ec.europa.eu>
2025-04-28 23:12:24 +0200

    --- FRONT END 101  ---

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const request = require('request');
 const TransactionPool = require('./wallet/transaction-pool');
 const Wallet = require('./wallet');
 const TransactionMiner = require('./app/transaction-miner');
+const path = require('path');
 
 const app = express();
 const blockchain = new Blockchain();
@@ -22,6 +23,7 @@ const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 // setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.get('/api/blocks', (req, res) => {
   res.json(blockchain.chain);
@@ -83,6 +85,10 @@ app.get('/api/wallet-info', (req, res) => {
   });
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+});
+
 const syncWithRootState = () => {
   request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
     if (!error && response.statusCode === 200) {
@@ -102,6 +108,47 @@ const syncWithRootState = () => {
     }
   });
 };
+
+
+const walletFoo = new Wallet();
+const walletBar = new Wallet();
+
+const generateWalletTransaction = ({ wallet, recipient, amount }) => {
+  const transaction = wallet.createTransaction({
+    recipient, amount, chain: blockchain.chain
+  });
+
+  transactionPool.setTransaction(transaction);
+};
+
+const walletAction = () => generateWalletTransaction({
+  wallet, recipient: walletFoo.publicKey, amount: 5
+});
+
+const walletFooAction = () => generateWalletTransaction({
+  wallet: walletFoo, recipient: walletBar.publicKey, amount: 10
+});
+
+const walletBarAction = () => generateWalletTransaction({
+  wallet: walletBar, recipient: wallet.publicKey, amount: 15
+});
+
+for (let i=0; i<10; i++) {
+  if (i%3 === 0) {
+    walletAction();
+    walletFooAction();
+  } else if (i%3 === 1) {
+    walletAction();
+    walletBarAction();
+  } else {
+    walletFooAction();
+    walletBarAction();
+  }
+
+  transactionMiner.mineTransactions();
+}
+
+
 
 let PEER_PORT;
 
