2e1bab82eae49d0c978a3e71e034444a9542dd79
chaalej <alejandro.chavez@ext.ec.europa.eu>
2025-04-29 20:39:48 +0200

    --- FRONT END DONE (Pubnub)  ---

--- app/pubsub.js
+++ app/pubsub.js
@@ -1,81 +1,90 @@
-const redis = require('redis');
+const PubNub = require('pubnub');
+
+const credentials = {
+  publishKey: 'pub-c-8939c2e2-53d1-4049-b337-b385c4bd7677',
+  subscribeKey: 'sub-c-d55c8042-5ecd-48ea-b247-638ca2e727c7',
+  secretKey: 'sec-c-Y2VjM2ViMjUtMDI1Mi00MWI0LTliMWItZDhlZDA4MDQ5MjVj'
+};
 
 const CHANNELS = {
-    TEST: 'TEST',
-    BLOCKCHAIN: 'BLOCKCHAIN',
-    TRANSACTION: 'TRANSACTION'
-  };
-  
-  class PubSub {
-    constructor({ blockchain, transactionPool }) {
-      this.blockchain = blockchain;
-      this.transactionPool = transactionPool;
-  
-    this.publisher = redis.createClient({
-        url: 'redis://localhost:12000'
-    });
-    this.subscriber = redis.createClient({
-        url: 'redis://localhost:12000'
-    });
-  
-      this.subscribeToChannels();
-  
-      this.subscriber.on(
-        'message',
-        (channel, message) => this.handleMessage(channel, message)
-      );
-    }
-  
-    handleMessage(channel, message) {
-      console.log(`Message received. Channel: ${channel}. Message: ${message}`);
-  
-      const parsedMessage = JSON.parse(message);
-  
-      switch(channel) {
-        case CHANNELS.BLOCKCHAIN:
-          this.blockchain.replaceChain(parsedMessage, true, () => {
-            this.transactionPool.clearBlockchainTransactions({
-               chain: parsedMessage
-            });
-          });
-          break;
-        case CHANNELS.TRANSACTION:
-          this.transactionPool.setTransaction(parsedMessage);
-          break;
-        default:
-          return;
-      }
-    }
-  
-    subscribeToChannels() {
-      Object.values(CHANNELS).forEach(channel => {
-        this.subscriber.subscribe(channel);
-      });
-    }
-  
-    publish({ channel, message}) {
-      this.subscriber.unsubscribe(channel, () => {
-        this.publisher.publish(channel, message, () => {
-          this.subscriber.subscribe(channel);
-        });
-      });
-    }
-  
-    broadcastChain() {
-      this.publish({
-        channel: CHANNELS.BLOCKCHAIN,
-        message: JSON.stringify(this.blockchain.chain)
-      });
-    }
+  TEST: 'TEST',
+  BLOCKCHAIN: 'BLOCKCHAIN',
+  TRANSACTION: 'TRANSACTION'
+};
 
-    broadcastTransaction(transaction) {
-      this.publish({
-        channel: CHANNELS.TRANSACTION,
-        message: JSON.stringify(transaction)
-      })
-    }
 
+class PubSub {
+  constructor({ blockchain, transactionPool }) {
+
+    this.blockchain = blockchain;
+
+    this.transactionPool = transactionPool;
+
+    this.pubnub = new PubNub(credentials);
+
+    this.pubnub.subscribe({ channels: Object.values(CHANNELS) });
+
+    this.pubnub.addListener(this.listener());
   }
-  
-  module.exports = PubSub;
-  
+
+  handleMessage(channel, message) {
+    console.log(`Message received. Channel: ${channel}. Message: ${message}`);
+
+    const parsedMessage = JSON.parse(message);
+
+    switch(channel) {
+      case CHANNELS.BLOCKCHAIN:
+        this.blockchain.replaceChain(parsedMessage, true, () => {
+          this.transactionPool.clearBlockchainTransactions({
+             chain: parsedMessage
+          });
+        });
+        break;
+      case CHANNELS.TRANSACTION:
+        this.transactionPool.setTransaction(parsedMessage);
+        break;
+      default:
+        return;
+    }
+  }
+
+  subscribeToChannels() {
+    Object.values(CHANNELS).forEach(channel => {
+      this.subscriber.subscribe(channel);
+    });
+  }
+
+  listener() {
+    return {
+      message: messageObject => {
+        const { channel, message } = messageObject;
+
+        this.handleMessage(channel, message);
+      }
+    };
+  }
+
+  publish({ channel, message }) {
+    this.pubnub.publish({ channel, message });
+  }
+
+  broadcastChain() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain)
+    });
+  }
+
+  broadcastTransaction(transaction) {
+    this.publish({
+      channel: CHANNELS.TRANSACTION,
+      message: JSON.stringify(transaction)
+    })
+  }
+
+}
+
+// const testPubSub = new PubSub();
+// testPubSub.publish({ channel: CHANNELS.TEST, message: 'hello pubnub 101' });
+
+module.exports = PubSub;
